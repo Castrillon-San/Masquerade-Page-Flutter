850991dbbc4e44580b9e6e38d4d27991751493c4
Santiago <santiago.castrilllong@upb.edu.co>
2022-11-19 11:25:40 -0500

    Avance vistas

--- masquerade_page/lib/util/custom_text_style.dart
+++ masquerade_page/lib/util/custom_text_style.dart
@@ -2,27 +2,36 @@ import 'package:flutter/cupertino.dart';
 import 'package:masquerade_page/util/custom_color.dart';
 
 class CustomTextStyles {
-  static TextStyle headingMedium = TextStyle(
-    fontSize: 12,
-    fontWeight: FontWeight.bold,
-    color: CustomColors.black,
-  );
+  // static TextStyle headingMedium = TextStyle(
+  //   fontSize: 12,
+  //   fontWeight: FontWeight.bold,
+  //   color: CustomColors.black,
+  // );
 
-  static TextStyle parraphMedium = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.w400,
-    color: CustomColors.black,
-  );
+  // static TextStyle parraphMedium = TextStyle(
+  //   fontSize: 18,
+  //   fontWeight: FontWeight.w400,
+  //   color: CustomColors.black,
+  // );
 
-  static TextStyle headingGigant = TextStyle(
-    fontSize: 80,
-    fontWeight: FontWeight.bold,
-    color: CustomColors.white,
-  );
+  // static TextStyle headingGigant = TextStyle(
+  //   fontSize: 80,
+  //   fontWeight: FontWeight.bold,
+  //   color: CustomColors.white,
+  // );
 
-  static TextStyle titleAppBar = TextStyle(
+  static const TextStyle titleAppBar = TextStyle(
       fontSize: 70, fontFamily: 'CinzelBlack', color: CustomColors.white);
 
-  static TextStyle buttonText = TextStyle(
+  static const TextStyle buttonText = TextStyle(
       fontSize: 26, fontFamily: 'CinzelBold', color: CustomColors.white);
+
+  static const TextStyle subtitle = TextStyle(
+      fontSize: 46,
+      fontFamily: 'CinzelBlack',
+      color: CustomColors.white,
+      decoration: TextDecoration.underline);
+
+  static const TextStyle content = TextStyle(
+      fontSize: 20, fontFamily: 'CinzelRegular', color: CustomColors.white);
 }
--- masquerade_page/lib/vistas/home_page.dart
+++ masquerade_page/lib/vistas/home_page.dart
@@ -4,7 +4,6 @@ import 'package:masquerade_page/util/custom_text_style.dart';
 import 'package:masquerade_page/vistas/appbar.dart';
 import 'package:masquerade_page/widgets/custom_background_scaffold.dart';
 import 'package:masquerade_page/widgets/custom_row_pick.dart';
-import 'package:masquerade_page/widgets/custom_background_scaffold.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -31,7 +30,8 @@ class _MyHomePageState extends State<MyHomePage> {
               boxShadow: [
                 BoxShadow(
                   color: CustomColors.black,
-                  spreadRadius: -100.100,
+                  offset: Offset(-100, -100),
+                  spreadRadius: -10.0,
                   blurRadius: 12.0,
                 )
               ],
@@ -46,12 +46,55 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           BaseLayout(
             imageBackground: "assets/images/TownBkgnd.png",
+            myChild: Center(
+              child: Row(children: <Widget>[
+                const Spacer(flex: 1),
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: 100,
+                    ),
+                    generarTextoPersonalizado(
+                        myText: "Sinópsis",
+                        textStyle: CustomTextStyles.subtitle),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    generarTextoPersonalizado(
+                        myText:
+                            "Lo despierta en un tren sin recuerdo alguno \nsobre quien es, en su rostro una máscara que no \nse puede quitar. El conductor del tren, Caronte, \nlo lleva a un lugar con la promesa de que allá \npuede encontrar quien es. \n\nLo se dedica a explorar el pueblo de Liberio en \nbusca de respuestas; extrañamente toda persona \ncon la que se encuentra tiene también una \nmáscara y aparentan estar sumamente desorientados. \nTras pasar su primer día, Lo se da cuenta que \naquellas personas olvidan con el paso de la \nnoche todo lo que había sucedido el día anterior, y \nes aquí donde comenzará su travesía en \ndescubrir la oscura realidad detrás de este \nmisterioso pueblo.",
+                        textStyle: CustomTextStyles.content)
+                  ],
+                ),
+                const Spacer(flex: 2),
+                Image.asset(
+                  "assets/images/IMG_4093.PNG",
+                  width: 500,
+                ),
+                const Spacer(flex: 1),
+              ]),
+            ),
+          ),
+          //Vista de Trailer
+          BaseLayout(
+            imageBackground: "assets/images/BellBkgnd.png",
+            myChild: Center(
+                child: Column(children: <Widget>[
+              generarTextoPersonalizado(
+                  myText: "Tráiler", textStyle: CustomTextStyles.subtitle),
+            ])),
+          ),
+          BaseLayout(
+            imageBackground: "assets/images/TownBkgnd.png",
             myChild: Column(children: const <Widget>[]),
-          )
-          //child: Column(),
-          //),
+          ),
         ],
       ),
     ));
   }
+}
+
+Widget generarTextoPersonalizado(
+    {required String myText, required TextStyle textStyle}) {
+  return Text(myText, style: textStyle);
 }
